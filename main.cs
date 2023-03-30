8e6fc22a53849cd809cacbc2c2acb41c79690eb2
PrzemSob <126515804+PrzemSob@users.noreply.github.com>
2023-03-30 18:15:47 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,38 +6,44 @@ class Program
     {
         // Uzytkownik wprowadza A,B i C
         Console.WriteLine("Podaj A:");
-        int psA = Convert.ToInt32(Console.ReadLine());
+        double psA = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Podaj B:");
-        int psB = Convert.ToInt32(Console.ReadLine());
+        double psB = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Podaj C:");
-        int psC = Convert.ToInt32(Console.ReadLine());
+        double psC = double.Parse(Console.ReadLine());
 
 
         //deklaracja zmiennych dla delty i x-ow
-        int psdelta = 0;
-        int psx1 = 0;
-        int psx2 = 0;
-        int psx0 = 0;
-        int psx = 0;
+        double psdelta = 0;
+        double psx1 = 0;
+        double psx2 = 0;
+        double psx0 = 0;
+        double psx = 0;
+        double pspier_z_del = 0;
+
 
 
         //jezelia a i c nie rowna sie 0 program oblicza delte
         if (psA != 0 && psC != 0)
         {
-            psdelta = psB * psB - 4 * (psA * psC);
-            //test 
-            Console.WriteLine(psdelta);
+            psdelta = (psB * psB) - 4 * psA * psC;
+
+            //print delty
+            Console.WriteLine($"Δ = {psdelta}");
 
             //obliczenie x1 i x2 dla delty wiekszej od zera
             if (psdelta > 0)
             {
-                psx1 = Convert.ToInt32(-(psB) - Math.Sqrt(psdelta)) / 2 * psA;
-                psx2 = Convert.ToInt32(-(psB) + Math.Sqrt(psdelta)) / 2 * psA;
-                //test
-                Console.WriteLine(psx1);
-                Console.WriteLine(psx2);
+                pspier_z_del = Math.Sqrt(psdelta);
+
+                psx1 = (-psB - pspier_z_del) / (2 * psA);
+                psx2 = (-psB + pspier_z_del) / (2 * psA);
+
+                //print wyniku x1 i x2
+                Console.WriteLine($"x1 = {psx1}");
+                Console.WriteLine($"x2 = {psx2}");
             }
 
             //obliczenie x dla delty rownej 0
@@ -45,8 +51,8 @@ class Program
             {
                 psx0 = (-(psB)) / 2 * psA;
 
-                //teset
-                Console.WriteLine(psx0);
+                //print wyniku x
+                Console.WriteLine($"x = {psx0}");
             }
 
             //print informacji o braku rozwiązań dla delty mniejszej od zera
@@ -59,7 +65,8 @@ class Program
         //dla a roznego od zera i c = 0 rownanie jest liniowe ax-b=0
         else
         {
-            psx =
-            }
+            psx = -psB / psA;
+            Console.WriteLine($"x = {psx}");
+        }
     }
 }
